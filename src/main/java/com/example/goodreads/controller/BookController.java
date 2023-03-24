01666d0d583c44c1fa2006aa8fb3b96879a719c4
MvrPrathyush <mvrintech@outlook.com>
2023-03-24 20:02:46 +0000

    goodreads

--- src/main/java/com/example/goodreads/controller/BookController.java
+++ src/main/java/com/example/goodreads/controller/BookController.java
@@ -1,21 +1,23 @@
 package com.example.goodreads.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.goodreads.model.Book;
-import com.example.goodreads.service.BookService;
-
-import java.util.*;
+import com.example.goodreads.service.BookH2Service;
 
 @RestController
 class BookController {
-    BookService bookService = new BookService();
+   @Autowired
+   BookH2Service bookService;
 
     @GetMapping("/books")
     public ArrayList<Book> getBooks() {
